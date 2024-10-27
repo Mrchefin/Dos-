87444e7a1c67e5ca5d55cc64fce6b608d63fa545
Mrchefin <158236891+Mrchefin@users.noreply.github.com>
2024-10-27 13:59:38 -0300

    Add files via upload
    Cmd sh bibliotecas.sh

---
+++ bibliotecas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+pip install rich
+pip install pillow
+pip install requests
+pip install itertools
+pip install time
+pip install string
+pip install os
+pip install random
+pip install pyfiglet 
+clear
+
+
+echo "blibliotecas instaladas 😋😋😋😋"
+sleep 3
+
+rm -rf bibliotecas.sh
+
+sleep 2
+echo " agora de o cmd python3 dos.py e aproveite espere 5 segundos"
+sleep 4
+clear
+sleep 2
+pyfiglet seu broxa
+
